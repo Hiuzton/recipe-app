a70dc63b32bd7dc94b716bfdf25ded459a1cfa59
Andrei <andreyka.gutsan@gmail.com>
2025-09-13 17:06:10 +0300

    Turn off dark mode

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import PrimeVue from 'primevue/config'
 import MyAuraPreset from './styles/themes/customTheme'
 import 'primeicons/primeicons.css'
 
+document.documentElement.classList.remove('dark')
+
 const app = createApp(App)
 
 app.use(PrimeVue, {
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: false, // 👈 This disables dark mode entirely
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
